2f7c2c6e56f5091f885a58b2c2af0faf36d251b5
Phyllipe <filipeetx@gmail.com>
2020-06-07 14:11:35 -0300

    Arrumando Entidade

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
+        public List<string> _mensagemValidacao { get; set; }
+
+        private List<string> mensagemValidacao {
+
+            get{ return _mensagemValidacao ?? (_mensagemValidacao = new List<string>()); }
+
+        }
+        protected void LimparMensagesValidacao()
+        {
+            mensagemValidacao.Clear();
+
+        }
+        protected void AdiconarCritica(string mensagem)
+        {
+            mensagemValidacao.Add(mensagem);
+
+        }
+        public abstract void Validacao();
+        protected bool EhValido
+        {
+            get { return !mensagemValidacao.Any(); }
+
+        }
+
+
         
     }
 }
--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,11 +1,12 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -19,5 +20,15 @@ namespace QuickBuy.Dominio.Entidades
         public int NumeroEndereco { get; set; }
         public int FormaPagamentoId { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
+
+        public override void Validacao()
+        {
+           
+            LimparMensagesValidacao();
+            if (!ItensPedido.Any())
+                AdiconarCritica("Crítica - Pedido não pode ficar sem item de pedido");
+            if(string.IsNullOrEmpty(Cep))
+                AdiconarCritica("Crítica - CEP deve estar preenchido");
+        }
     }
 }
