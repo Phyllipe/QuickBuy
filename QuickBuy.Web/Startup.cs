5e297dfb3ace5f4a9572000f891b70c6e1ef6557
Phyllipe <filipeetx@gmail.com>
2020-06-11 17:52:47 -0300

    preparativos para angular

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -29,7 +29,9 @@ namespace QuickBuy.Web
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             var connectionString = Configuration.GetConnectionString("ServidorPhyllipe");
-            services.AddDbContext<QuickBuyContexto>(option => option.UseLazyLoadingProxies() .UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio"))) ;
+            services.AddDbContext<QuickBuyContexto>(option => option.UseLazyLoadingProxies() 
+                                                                           .UseMySql(connectionString,
+                                                                                    m => m.MigrationsAssembly("QuickBuy.Repositorio"))) ;
 
             services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
 
